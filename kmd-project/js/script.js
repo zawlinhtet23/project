ca1fa67e7d48c1be25c64d03c7600178a624a475
Zaw Lin Htet <155418710+zawlinhtet23@users.noreply.github.com>
2024-02-07 09:17:12 +0630

    Add files via upload

---
+++ kmd-project/js/script.js
@@ -0,0 +1,12 @@
+function toggleFavorite(icon) {
+    icon.classList.toggle('clicked');
+    
+}
+
+var myCarousel = new bootstrap.Carousel(document.getElementById('carouselExample'), {
+    interval: 2000
+});
+
+function shoppingCart() {
+    
+}
